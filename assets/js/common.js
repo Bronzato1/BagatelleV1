8a0fea0609bc356cd359dc5f2a843f5a9dc1e4b6
Bronzato1 <azur.consult@gmail.com>
2023-08-03 10:15:50 +0200

    Commit (8th)

--- assets/js/common.js
+++ assets/js/common.js
@@ -389,7 +389,7 @@
 
   async function getPredictionFromUrl(imageUrl, base64Data)
   {
-      const apiUrl = 'http://cors-anywhere.herokuapp.com/https://api.replicate.com/v1/predictions';
+      const apiUrl = 'https://cors-anywhere.herokuapp.com/https://api.replicate.com/v1/predictions';
       const apiToken = 'r8_WsSrh8w9Qp9YQwahxuiNnbWnF47T8rj3ZQ9vx';
       const version = '42fed1c4974146d4d2414e2be2c5277c7fcf05fcc3a73abf41610695738c1d7b';
       const progressLabel = document.querySelector('#progressLabel');
